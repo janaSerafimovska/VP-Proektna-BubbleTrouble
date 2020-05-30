00da32dbc10b6a32f72cc5fc59849b748b4ac6d2
Митровски Јаков <jakov.mitrovski@students.finki.ukim.mk>
2020-05-30 13:36:50 +0200

    Add LevelOne class, Update Level class

---
+++ LevelOne.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleTrouble
+{
+
+    //Klasa koja e izvedena od klasata Level i go opisuva prvoto nivo.
+    public class LevelOne : Level
+    {
+        public LevelOne(int LevelID, Point StartingPositions) : base(LevelID, StartingPositions)
+        {
+            
+        }
+
+        //metod koj dodava prepreka vo listata od prepreki.
+        public override void AddObstacle(Obstacle ToAdd)
+        {
+            Obstacles.Add(ToAdd);
+        }
+
+        //Metod koj generira prepreki za dadenoto nivo.
+        public override void GenerateObstacles()
+        {
+            //No obstacles in lvl 1.
+        }
+
+        //Metod koj vrakja na koe nivo sme momentalno.
+        public override int GetLevel()
+        {
+            return LevelID;
+        }
+    }
+}
