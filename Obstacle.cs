bc9a3700ed4588cde4a10085465e9171bbfaaa22
janaSerafimovska <62503690+janaSerafimovska@users.noreply.github.com>
2020-05-30 12:01:59 +0200

    add Player class

--- Obstacle.cs
+++ Obstacle.cs
@@ -10,13 +10,27 @@ namespace BubbleTrouble
     public class Obstacle
     {
         public Color ObstacleColor { get; set; }
-        public Point Point1 { get; set; }
-        public Point Point2 { get; set; }
-        public Point Point3 { get; set; }
-        public Point Point4 { get; set; }
+        public Point TopLeft { get; set; }
+        public Point TopRight { get; set; }
+        public Point BottomLeft { get; set; }
+        public Point BottomRight { get; set; }
 
-        public void DrawObstacle()
+        public void DrawObstacle(Color color, Point topLeft, Point topRight, Point bottomRight, Point bottomLeft, Graphics canvas)
         {
+            Brush brush = new SolidBrush(color);
+            Pen pen = new Pen(color, 1);
+            ObstacleColor = color;
+            TopRight = topRight;
+            TopLeft = topLeft;
+            BottomLeft = bottomLeft;
+            BottomRight = bottomRight;
+
+            Point[] obstacle = { TopLeft, TopRight, BottomLeft, BottomRight };
+            canvas.DrawPolygon(pen, obstacle);
+            canvas.FillPolygon(brush, obstacle);
+
+            brush.Dispose();
+            pen.Dispose();
 
         }
     }
---
+++ Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleTrouble
+{
+    public sealed class Player
+    {
+        private static Player instance = null;
+        private static readonly object padlock = new object();
+
+        Player()
+        {            
+        }
+
+        public static Player Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Player();
+                    }
+                    return instance;
+                }
+            }
+
+        }
+    }
+ 
+}
