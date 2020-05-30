f392e937236e37a1c2de76ccf9f20f9e1af90f1a
Зографска Александра <aleksandra.zografska@students.finki.ukim.mk>
2020-05-30 23:40:22 +0200

    ball class updated

--- Ball.cs
+++ Ball.cs
@@ -7,12 +7,39 @@ using System.Threading.Tasks;
 
 namespace BubbleTrouble
 {
-    //pishi ovde komentar
+    //Glavna klasa ball od koja ke se izveduvaat topki od razlicna boja i golemina
     public class Ball
     {
+        //dimenzii na prozorecot za igra
+
+        //osbovni karakteristiki na topkata
+        public int Radius { get; set; }
+        public Point Center { get; set; }
+        public Color Color { get; set; }
+
+        //brzini po x i y oska
+        public int vx { get; set; }
+        public int vy { get; set; }
+
+        public Ball(int Radius,Point Center,Color Color){
+            this.Color = Color;
+            this.Center = Center;
+            this.Radius = Radius;
+
+            vx = 5;
+            vy = 5;
+
+        }
         public void Draw(Graphics g)
         {
+            Brush brush = new SolidBrush(Color);
+            g.FillEllipse(brush, Center.X - Radius, Center.Y - Radius, Radius * 2, Radius * 2);
+            brush.Dispose();
 
+        }
+        public bool HitsFloor() { }
+        public void Move() { 
+        
         }
     }
 
